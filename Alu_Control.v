3e94de9b7dd261a63c91c3cb2c40548c56a5bc11
all888168 <66481118+all888168@users.noreply.github.com>
2020-06-20 17:35:10 -0700

    add a comment for SLTU
    add a comment for SLTU base on the latest code updated

--- Alu_Control.v
+++ Alu_Control.v
@@ -1,60 +1,29 @@
 module ALU_Control(
     input   [2:0]   Funct3,
     input   [6:0]   Funct7,
-    input   [2:0]   ALUOp,
+    input   [1:0]   ALUOp,
     output  [4:0]   ALUSignal
 )
 
-
-    parameter RTYPE  = 3'b000;
-    parameter ITYPE  = 3'b001;
-    parameter STYPE  = 3'b010;
-    parameter BTYPE  = 3'b011;
-    parameter UTYPE  = 3'b100;
-    parameter JTYPE  = 3'b101;
-    parameter LITYPE = 3'b110;
-    parameter LJTYPE = 3'b111;
-
     always @(*) begin 
         case (ALUOp):
-            `RTYPE : begin
+            2'b00 : ALUSignal = `ADD;
+            2'b01 : ALUSignal = `SUB;
+            2'b10 : begin
                 case (Funct3):
                     3'b000: begin
                         if (Funct7 == 7'b0000000) ALUSignal = `ADD;
                         else ALUSignal = `SUB;
                     end
                     3'b001: ALUSignal = `SLL;
-                    3'b010: ALUSignal = `SLT;
-                    3'b011: ALUSignal = `SLTU;
-                    3'b100: ALUSignal = `XOR;
-                    3'b101: begin
-                        if (Funct7 == 7'b0000000) ALUSignal = `SRL;
-                        else ALUSignal = `SRA;
-                    end
+                    3'b010: ALUSignal = `SLTU;      //?? SLTU or SLTI?
+                    3'b011: ALUSignal = `XOR;
+                    3'b100: ALUSignal = `SRL;
+                    3'b101: ALUSignal = `SRA;
                     3'b110: ALUSignal = `OR;
                     3'b111: ALUSignal = `AND;
                 endcase
             end
-            `ITYPE : begin
-                case (Funct3):
-                    3'b000: ALUSignal = `ADD;
-                    3'b001: ALUSignal = `SLL;
-                    3'b010: ALUSignal = `SLT;
-                    3'b011: ALUSignal = `SLTU;
-                    3'b100: ALUSignal = `XOR;
-                    3'b101: begin
-                        if (Funct7 == 7'b0000000) ALUSignal = `SRL;
-                        else ALUSignal = `SRA;
-                    end
-                    3'b110: ALUSignal = `OR;
-                    3'b111: ALUSignal = `AND;
-                endcase
-            end
-            `STYPE :  ALUSignal = `ADD;
-            `BTYPE :  ALUSignal = `SUB; // only BEQ
-            `UTYPE :  ALUSignal = `ADD; // only AUIPC
-            `JTYPE :  ALUSignal = `ADD; // only JAL
-            `JITYPE : ALUSignal = `ADD; // only JALR
         endcase
     end
 endmodule

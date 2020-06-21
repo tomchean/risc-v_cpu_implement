ab578c1f899af1261353ba3bd9bd5f10fea85435
all888168 <all888168@gmail.com>
2020-06-20 23:59:06 -0700

    Update Alu.v
    Fixed some errors found during compiling

--- Alu.v
+++ Alu.v
@@ -1,43 +1,59 @@
+`define ADD 10
+`define SUB 10
+`define AND 10
+`define OR 10
+`define SLTU 10
+`define XOR 10
+`define SLL 10
+`define SRL 10
+`define SRA 10
+
 module ALU(
     input   [4:0]   ALUSignal,
-    input   [31:0]  ALUinA,
-    input   [31:0]  ALUinB,
-    output  [31:0]  ALUout,
-    output  [1:0]   Flags
-)
+    input   [31:0]  AiA,
+    input   [31:0]  AiB,
+    output  [31:0]  Aout,
+    input   ACin,           // ALU carry input
+    output  ACout,          // ALU carry output  
+    output  AZout,          // ALU Zero output    
+);      
 
     always @(*) begin 
-        case (ALUSignal):
-            ADD: begin
-                ALUout = ALUinA + ALUinB;       // Addition
+        case(ALUSignal)
+            `ADD: begin
+                {ACout, Aout} = AiA + AiB + ACin; // Addition
             end
-            SUB: begin
-                ALUout = ALUinA - ALUinB;       // Substration
+            `SUB: begin
+                Aout = AiA - AiB;       // Substration
             end
-            AND: begin
-                ALUout = ALUinA & ALUinB;       // Bitwise AND
+            `AND: begin
+                Aout = AiA & AiB;       // Bitwise AND
             end
-            OR: begin
-                ALUout = ALUinA | ALUinB;       // Bitwise OR
+            `OR: begin
+                Aout = AiA | AiB;       // Bitwise OR
             end
-            SLTU: begin                         //?? SLTU or SLTI?
-                if (ALUinA < ALUinB)
-                    ALUout = 32'b1;
+            `SLTU: begin                //?? SLTU or SLTI?
+                if (AiA < AiB)
+                    Aout = 32'b1;
                 else
-                    ALUout = 32'b0;
+                    Aout = 32'b0;
             end            
-            XOR: begin
-                ALUout = ALUinA ^ ALUinB;       
+            `XOR: begin
+                Aout = AiA ^ AiB;       // XOR
             end
-            SLL: begin
-                ALUout = ALUinA << ALUinB;      // Shift ALUinB bits left
+            `SLL: begin
+                Aout = AiA << AiB;      // Shift AiB bits left
             end
-            SRL: begin
-                ALUout = ALUinA >>> ALUinB;     // Arithmetic shift ALUinB bits right
+            `SRL: begin
+                Aout = AiA >>> AiB;     // Arithmetic shift AiB bits right
             end
-            SRA: begin
-                ALUout = ALUinA >> ALUinB;      // Shift ALUinB bits right
+            `SRA: begin
+                Aout = AiA >> AiB;      // Shift AiB bits right
             end
         endcase
+    end
+    always @(Aout) begin
+        if(Aout==0)
+            AZout = 1;
     end
 endmodule
